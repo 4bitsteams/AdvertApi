ad29fc37b7d1a51803aa5a7d77f8af049022fc43
rubel islam <rubelislam301@gmail.com>
2021-07-02 17:29:34 +0600

    Design  Database Service

--- Services/DynamoDBAdvertStorage.cs
+++ Services/DynamoDBAdvertStorage.cs
@@ -1,4 +1,7 @@
 ﻿using AdvertApi.Models;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,28 @@ namespace AdvertApi.Services
 {
     public class DynamoDBAdvertStorage : IAdvertStorageService
     {
-        public Task<string> Add(AdvertModel model)
+        private readonly IMapper _mapper;
+
+        public DynamoDBAdvertStorage(IMapper mapper)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+        }
+        public async Task<string> Add(AdvertModel model)
+        {
+            var dbModel = _mapper.Map<AdvertDbModel>(model);
+            dbModel.Id = new Guid().ToString();
+            dbModel.CreationDateTime = DateTime.UtcNow;
+            dbModel.Status = AdvertStatus.Pending;
+
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    await context.SaveAsync(dbModel);
+                }
+            }
+
+            return dbModel.Id;
         }
 
         public Task<bool> Confirm(ConfirmAdvertModel model)
