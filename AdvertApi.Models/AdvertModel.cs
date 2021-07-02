474448923abff6c520befeabe60dabf9f554ff08
rubel islam <rubelislam301@gmail.com>
2021-07-02 15:52:47 +0600

    Add AdvertApi Model Class Library

---
+++ AdvertApi.Models/AdvertModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AdvertApi.Models
+{
+    public class AdvertModel
+    {
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public Double Price { get; set; }
+    }
+}
